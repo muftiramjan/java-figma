dcf98dc5e118004e16e7ebf4298c6c34b8c2952d
mufti ramjan <alibinalaluddin@gmail.com>
2024-02-13 09:13:56 +0600

    add sacses

---
+++ java.js
@@ -0,0 +1,35 @@
+function tanangelarya(){
+   const traiangel=document.getElementById('taiangel-area');
+   const tttt= traiangel.value;
+   const bbbbb=parseInt(tttt)
+   console.log(bbbbb);
+
+   // 2
+   const tangaHight=document.getElementById('tain-hight');
+   const gggg=tangaHight.value;
+   console.log( gggg);
+   // 3
+   const area =0.5 * bbbbb * gggg;
+   console.log('my name is ramjan',area);
+   // 4
+   const areaa=document.getElementById('spnAria');
+   
+   areaa.innerText=area;
+}
+// secend
+function rectAngel(){
+  const rectAnge2=document.getElementById('rectangel-f-input');
+ const sssss= rectAnge2.value;
+ const gggg = parseInt(sssss)
+ console.log(gggg);
+//  2
+const rectangil222=document.getElementById('rectangel-s-input');
+const hhhhh=rectangil222.value;
+console.log(hhhhh);
+// 3
+const total=gggg*hhhhh;
+console.log(total)
+}
+
+
+   
